aebfd0ac9430ebdb2f1c833b649c09952ffc6292
Shalini Sharma <shalinisharma@Mac.lan>
2025-08-03 13:43:27 +0530

    eslient error resolve again

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -15,27 +15,21 @@
 
 // export default eslintConfig;
 
-import { dirname } from "path";
-import { fileURLToPath } from "url";
-import { FlatCompat } from "@eslint/eslintrc";
-
-const __filename = fileURLToPath(import.meta.url);
-const __dirname = dirname(__filename);
-
+import { FlatCompat } from '@eslint/eslintrc'
+ 
 const compat = new FlatCompat({
-  baseDirectory: __dirname,
-});
-
+  // import.meta.dirname is available after Node.js v20.11.0
+  baseDirectory: import.meta.dirname,
+})
+ 
 const eslintConfig = [
-  ...compat.extends("next/core-web-vitals", "next/typescript"),
-
-  // ✅ Add custom rules here
-  {
+  ...compat.config({
+    extends: ['next'],
     rules: {
-      "@next/next/no-img-element": "off",
-      "react-hooks/exhaustive-deps": "warn", // or "off"
+      'react/no-unescaped-entities': 'off',
+      '@next/next/no-page-custom-font': 'off',
     },
-  },
-];
-
-export default eslintConfig;
+  }),
+]
+ 
+export default eslintConfig
